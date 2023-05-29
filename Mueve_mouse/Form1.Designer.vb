ffdc768e83f8109511b7ff7ee4a4bbe003fc24a9
javierhgs <41153485+javierhgs@users.noreply.github.com>
2023-05-29 15:11:58 -0300

    Initial commit

---
+++ Mueve_mouse/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnIniciar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.txtSegundos = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnIniciar
+        '
+        Me.btnIniciar.Location = New System.Drawing.Point(70, 89)
+        Me.btnIniciar.Name = "btnIniciar"
+        Me.btnIniciar.Size = New System.Drawing.Size(138, 49)
+        Me.btnIniciar.TabIndex = 0
+        Me.btnIniciar.Text = "Iniciar"
+        Me.btnIniciar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(71, 155)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(136, 49)
+        Me.btnCancelar.TabIndex = 1
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'txtSegundos
+        '
+        Me.txtSegundos.Location = New System.Drawing.Point(92, 41)
+        Me.txtSegundos.Name = "txtSegundos"
+        Me.txtSegundos.Size = New System.Drawing.Size(104, 20)
+        Me.txtSegundos.TabIndex = 2
+        Me.txtSegundos.Text = "30"
+        Me.txtSegundos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(89, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Cantidad de segundos"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSegundos)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnIniciar)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Mueve ese Mouse"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnIniciar As System.Windows.Forms.Button
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+    Friend WithEvents txtSegundos As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Mueve_mouse/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+    ' Importar la función SetCursorPos de la API de Windows
+    <DllImport("user32.dll", SetLastError:=True)>
+    Private Shared Function SetCursorPos(ByVal X As Integer, ByVal Y As Integer) As Boolean
+    End Function
+
+    Private timer As Timer = New Timer()
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        ' Configurar el temporizador
+        timer.Interval = 1000 ' Intervalo en milisegundos
+        AddHandler timer.Tick, AddressOf Timer_Tick
+
+        ' Establecer la propiedad FormBorderStyle en FixedSingle
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+    End Sub
+
+    Private Sub btnIniciar_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnIniciar.Click
+        Dim segundos As Double
+        If Double.TryParse(txtSegundos.Text, segundos) Then
+            If segundos > 0 AndAlso segundos <= 3600 Then ' Limitar los segundos a un rango razonable (de 0 a 3600)
+                ' Iniciar el temporizador
+                timer.Interval = CInt(segundos * 1000) ' Convertir segundos a milisegundos
+                timer.Start()
+                btnIniciar.Enabled = False
+                txtSegundos.Enabled = False
+            Else
+                MessageBox.Show("Por favor, ingresa un valor válido para los segundos (entre 0 y 3600).")
+            End If
+        Else
+            MessageBox.Show("Por favor, ingresa un valor numérico válido para los segundos.")
+        End If
+    End Sub
+
+    Private Sub Timer_Tick(ByVal sender As Object, ByVal e As EventArgs)
+        ' Mover el mouse a una posición aleatoria en la pantalla
+        Dim rand As Random = New Random()
+        Dim x As Integer = rand.Next(Screen.PrimaryScreen.Bounds.Width)
+        Dim y As Integer = rand.Next(Screen.PrimaryScreen.Bounds.Height)
+        SetCursorPos(x, y)
+    End Sub
+
+    Private Sub btnCancelar_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnCancelar.Click
+        ' Detener el temporizador
+        timer.Stop()
+        btnIniciar.Enabled = True
+        txtSegundos.Enabled = True
+    End Sub
+End Class
